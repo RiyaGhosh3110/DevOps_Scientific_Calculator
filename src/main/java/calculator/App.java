d04881d5663689f3aef6d8acec62ee30a6e17ac6
parijat <parijatmoulik11@gmail.com>
2021-03-13 15:55:17 +0530

    3 stages

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 
 public class App
 {
-    private static final Logger logger = LogManager.getLogger(Calculator.class);
+    private static final Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String args[])
     {
